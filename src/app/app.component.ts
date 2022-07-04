9d8f60f86194287ebe5a1f8bb14281f3a1f4fc5d
lily <raichek23@gmail.com>
2022-07-05 01:31:28 +0900

    View timeline, Add comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { Comment } from "./class/comment";
+import { User } from "./class/user";
+
+const CURRENT_USER: User = new User(1, '五十川 洋平');
+const ANOTHER_USER: User = new User(2, '竹井 賢治');
+
+const COMMENTS: Comment[] = [
+  new Comment(ANOTHER_USER, 'Hello!'),
+  new Comment(ANOTHER_USER, 'How are you?'),
+  new Comment(CURRENT_USER, 'Good!'),
+  new Comment(CURRENT_USER, 'What is todays lunch?')
+]
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
+  currentUser = CURRENT_USER;
+  comment = '';
+
+  addComment(comment: string): void {
+    if (comment) {
+      this.comments.push(new Comment(this.currentUser, comment));
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentDatePipe } from './pipes/comment-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentDatePipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
